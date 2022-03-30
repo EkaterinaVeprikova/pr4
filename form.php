0d5f5d184752d3d5ce8bb290c913add55b4483d3
kater.rr <90448920+EkaterinaVeprikova@users.noreply.github.com>
2022-03-30 19:58:18 +0300

    Update form.php

--- form.php
+++ form.php
@@ -3,7 +3,7 @@
     <style>
 /* Сообщения об ошибках и поля с ошибками выводим с красным бордюром. */
 .error {
-  border: 5px solid red;
+  border: 5px solid transparent red;
 }
     </style>
   </head>
