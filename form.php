946dbf01269e796ddf04a51440fb5c8e13374e8b
kater.rr <90448920+EkaterinaVeprikova@users.noreply.github.com>
2022-03-30 18:27:15 +0300

    Update form.php

--- form.php
+++ form.php
@@ -23,10 +23,10 @@ if (!empty($messages)) {
 // и задавая начальные значения элементов ранее сохраненными.
 ?>
 
-   <!- <form action="" method="POST">
+   <! <form action="" method="POST">
       <input name="fio" <?php /* if ($errors['fio']) {print 'class="error"';} */ ?> value="<?php /* print $values['fio']; */ ?>" />
       <input type="submit" value="ok" />
-    </form> --?
+    </form> -->
    
     ===================================================================================================================
      <form action="" method="POST">
